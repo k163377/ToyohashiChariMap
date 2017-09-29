d70dda5e1bdfdbbe3123be156bf2a8edf2dd3db8
k163377 <k163377@edu.tut.ac.jp>
2017-09-30 07:13:36 +0900

    中島氏のコードをコピペ

--- app/src/main/java/com/doubutsunoyakata/toyohashicharimap/input/ReviewData.java
+++ app/src/main/java/com/doubutsunoyakata/toyohashicharimap/input/ReviewData.java
@@ -2,24 +2,51 @@ package com.doubutsunoyakata.toyohashicharimap.input;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
+import android.app.Activity;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
-public final class ReviewData {
+import static android.content.Context.MODE_PRIVATE;
+
+public final class ReviewData implements Serializable{
+    private static final long serialVersionUID = 6255752248513019207L;
+
     private final String dataID;//レビューデータの識別子、データを格納するファイル名としても利用する予定
     private final ArrayList<LatLng> latLngArray;//座標の行列
-    private int currentIndex;//入力の取り消し機能のため、今読んでいる場所を記憶]
+    private int currentIndex;//入力の取り消し機能のため、今読んでいる場所を記憶
+    private Date date;  // レビュー作成時の時刻
+
     private double time;//経過時間の計測
+    private static final DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");//時刻用フォーマット
 
     //プログラムから呼び出す方のコンストラクタ
-    public ReviewData(String ID){
-        this.dataID = ID;
+    public ReviewData(String name){
         latLngArray = new ArrayList<LatLng>();
-
         currentIndex = 0;
+        date = new Date( System.currentTimeMillis());//現在時間を取得
+        dataID = name + "_" + df.format(date);
     }
-    //ファイルから読み取る場合のコンストラクタ
-    /*まだ作ってない、後回しで*/
+    /**
+     * ファイルからデシリアライズして読み取るコンストラクタ
+     * @param activity 呼び出しもと
+     * @param dataID 識別子
+     */
+    public ReviewData(final Activity activity, final String dataID) {
+        ReviewData rd = inputDeserialize( activity, dataID);
+        this.dataID = rd.dataID;
+        this.latLngArray = rd.latLngArray;
+        this.currentIndex = rd.currentIndex;
+        this.date = rd.date;
+    }
 
     //ゲッター
     public ArrayList<LatLng> getLatLngArray(){
@@ -39,8 +66,7 @@ public final class ReviewData {
 
     //セッター
     public void addLatLng(LatLng p){
-        latLngArray.add(p);
-        currentIndex++;
+        latLngArray.add( currentIndex++, p);
     }
 
     //インデックスのアンドゥ
@@ -48,5 +74,46 @@ public final class ReviewData {
     //インデックスのリドゥ
     public void redoCurrentIndex(){ if(currentIndex < latLngArray.size()) currentIndex++; }
 
-    //シリアライズ、まだ
+    /**
+     * 自身をシリアライズしてファイルへと出力
+     * @param activity 呼び出しもと
+     * @return シリアライズ成功の可否
+     */
+    public boolean outputSerialize(final Activity activity) {
+        try {
+            // Androidアプリ用パス取得メソッド #Activity.class
+            FileOutputStream fos = activity.openFileOutput( dataID + ".dat", MODE_PRIVATE);
+            // オブジェクトのシリアライズ
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(this);
+            oos.close();
+            return true;
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return false;
+        }
+    }
+    /**
+     * ファイルをデシリアライズしてオブジェクトを取得
+     * @param activity 呼び出しもと
+     * @param dataID 識別子
+     */
+    public static ReviewData inputDeserialize(final Activity activity, final String dataID) {
+        try {
+            // Androidアプリ用パス取得メソッド #Activity.class
+            FileInputStream fis = activity.openFileInput( dataID +".dat");
+            // オブジェクトのデシリアライズ
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            ReviewData rd = (ReviewData)ois.readObject();
+            ois.close();
+            if (dataID.equals(rd.dataID)) {
+                return rd;
+            }
+            else
+                throw new IOException();
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
 }
