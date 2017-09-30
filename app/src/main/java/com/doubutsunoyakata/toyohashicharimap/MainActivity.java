724f5e6975425fbd63124cba4749f9ac63fb6d02
k163377 <k163377@edu.tut.ac.jp>
2017-09-30 12:54:58 +0900

    微修正 ボタンを追加し、経路の記録を可能に

--- app/src/main/java/com/doubutsunoyakata/toyohashicharimap/MainActivity.java
+++ app/src/main/java/com/doubutsunoyakata/toyohashicharimap/MainActivity.java
@@ -1,6 +1,10 @@
 package com.doubutsunoyakata.toyohashicharimap;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +18,22 @@ public class MainActivity extends AppCompatActivity {
     Button history;
     Button upload;
 
+    //GPSの権限を要求し、許可されたらMapへ移る
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: { //requestPermissions()の第2引数で指定した値
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {//許可された場合
+                    Intent intent[] = new Intent[1];
+                    intent[0] = new Intent(MainActivity.this, MapsActivity.class);
+                    startActivities(intent);
+                }else{//拒否された場合の処理
+                }
+                break;
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +42,19 @@ public class MainActivity extends AppCompatActivity {
         input = (Button) findViewById(R.id.Input);
         input.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent[] = new Intent[1];
-                intent[0] = new Intent(MainActivity.this, MapsActivity.class);
-                startActivities(intent);
+            public void onClick(View view) {//GPSからの入力への移行
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {//権限がまだ無い場合
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)) {//明示的に権限が拒否されていた時
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                    } else {//まだ聞いてなかったとき
+                        //与えても良いか聞く、onRequestPermissionsResultが答えを受ける
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                    }
+                }else{
+                    Intent intent[] = new Intent[1];
+                    intent[0] = new Intent(MainActivity.this, MapsActivity.class);
+                    startActivities(intent);
+                }
             }
         });
 
--- app/src/main/java/com/doubutsunoyakata/toyohashicharimap/input/MapsActivity.java
+++ app/src/main/java/com/doubutsunoyakata/toyohashicharimap/input/MapsActivity.java
@@ -1,15 +1,9 @@
 package com.doubutsunoyakata.toyohashicharimap.input;
 
-import android.Manifest;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
@@ -64,17 +58,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
         return bestLocation;
     }
-
+    //位置の記録
     private void startRecording(){
-        if (ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {//権限がまだ無い場合
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)) {//明示的に権限が拒否されていた時
-                //拒否されていた時の処理、なんかしらのメッセージを出すと良いと思われる、今は何も考えずにパーミッションを聞いておく
-                ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            } else {//まだ聞いてなかったとき
-                //与えても良いか聞く、onRequestPermissionsResultが答えを受ける
-                ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            }
-        }
         //一定時間ごとに
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask(){
@@ -88,6 +73,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     Log.d("Timer", currentLocation.toString());
                     LatLng p = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
                     rd.addLatLng(p);
+
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(p, 15));
 
                     //mMap.addMarker(new MarkerOptions().position(p).title("Start"));
                     //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(p, 15));
@@ -108,18 +95,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }
         else Log.d("StopButton", "timer null");
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 1: { //requestPermissions()の第2引数で指定した値
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {//許可された場合
-                }else{//拒否された場合の処理
-                }
-                break;
-            }
-        }
     }
 
     //ライフサイクル
@@ -147,36 +122,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             }
         });
-        /*ModeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startRecording();
-            }
-        });
-        StopButton = (Button) findViewById(R.id.StopButton);
-        StopButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopRecording();
-            }
-        });*/
 
         rd = new ReviewData("Dummy ID");
 
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        //権限の確認は、実際には使う場面でやるべきで、onCreateでやるのは不適
-        //GPS権限の確認
-        if (ContextCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {//権限がまだ無い場合
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)) {//明示的に権限が拒否されていた時
-                //拒否されていた時の処理、なんかしらのメッセージを出すと良いと思われる、今は何も考えずにパーミッションを聞いておく
-                ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            } else {//まだ聞いてなかったとき
-                //与えても良いか聞く、onRequestPermissionsResultが答えを受ける
-                ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            }
-        }else {//権限がある場合
-        }
     }
     @Override
     public void onDestroy() {
